9cdf45b3271ce4f6a065dc7c093c2f1b46c24a51
Itachoo <w.dapczynska@gmail.com>
2023-10-17 16:39:25 +0200

    add results

--- js/script.js
+++ js/script.js
@@ -24,10 +24,41 @@ let playerMove = 'nieznany ruch';
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
-else if (playerInput == 2){
+else if (playerInput == '2'){
     playerMove = 'papier';
 }
-else if (playerInput == 3){
+else if (playerInput == '3'){
     playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrywasz!');
+  }
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }
+if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ty przegrywasz!');
+  }
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty przegrywasz!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+  }
+if( playerMove == 'nieznany ruch'){
+    printMessage('Wybierz cyfrę od 1 do 3!');
+  }
